7937d071a05376a089439db31e4436152c2b43c3
Kate Fd <katesfadeeva@gmail.com>
2020-03-08 16:45:14 +0300

    init: start task Count Cats and Dream Team and What season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,19 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let name = [];
+    for(let i = 0; i < members.length; i++) {
+      if(typeof members[i] === "string") {
+        members[i] = members[i].replace(/\s+/g, '');
+        name.push(members[i][0].toUpperCase())
+      } 
+    }
+    let DreamTeam = name.sort().join('');  
+    if (DreamTeam == "") {
+      return false;
+    } else {
+      return DreamTeam;
+    }
+  } else {
+    return false;
+  }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,21 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let winter = [0,1,11];
+  let spring = [2,3,4];
+  let summer = [5,6,7];
+  let autumn = [8,9,10];
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  } else {
+    date.valueOf();
+    let month = date.getMonth(); 
+    if (winter.includes(month)) {
+      return 'winter';
+    } else if (spring.includes(month)) {
+      return 'spring';
+    } else if (summer.includes(month)) {
+      return 'summer';
+    } else if (autumn.includes(month)) {
+      return 'autumn';
+    }
+  }
 };
