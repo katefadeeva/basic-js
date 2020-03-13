1b77125b4167fd32e9258cb0d45f49cf2b28c919
Kate Fd <katesfadeeva@gmail.com>
2020-03-13 15:55:59 +0300

    init: start Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,23 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!sampleActivity) {
+    return false;
+  }
+  
+  if (typeof(sampleActivity)!='string') {
+    return false;
+  }
+
+  let sampleActivityNum = parseFloat(sampleActivity);
+
+  if (Number.isNaN(sampleActivityNum) || 
+      sampleActivityNum <= 0 || 
+      sampleActivityNum > MODERN_ACTIVITY) {
+        return  false;
+      } else {
+        return  Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivityNum)/0.693*HALF_LIFE_PERIOD);
+      }
+  
 };
