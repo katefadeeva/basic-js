30cf935bf194243e5cefd46856ac3290d623e57f
Kate Fd <katesfadeeva@gmail.com>
2020-03-19 12:24:02 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,26 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let repeatTimes = options.repeatTimes || 1;
+  let separator = options.separator || "+";
+  let addition = options.addition !== undefined ? options.addition : "";
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let additionSeparator = options.additionSeparator || "|";
+  
+  if (typeof(str) != 'string') {
+      str = String(str);
+  }
+
+  let r = "";
+  for (let i = 1; i < additionRepeatTimes; i++) {
+    r = r + addition + additionSeparator;
+  }
+  r += addition;
+
+  let newstr = "";
+  for (let k = 1; k < repeatTimes; k++) {
+    newstr = newstr + str + r + separator;
+  }
+  newstr += str + r;
+
+  return newstr;
 };
   
